c3e46e4e635fdea1903e491392b0d0c2e2b896bf
Zhongxun Pan <zpan0008@student.monash.edu>
2022-03-25 13:34:16 +1100

    Initial Commit

---
+++ YanMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YanMusic
+//
+//  Created by Rym- on 25/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
